12fc9fa1d77c5ef9ccf2b1014a2cb954ebfcc446
ddzik19 <ddzik09@gmail.com>
2025-01-05 15:34:39 +0000

    start + 8 mins

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,4 +1,3 @@
-// app/page.js (or any other page in the `app` directory)
 "use client";
 
 import { useQuery, gql } from "@apollo/client";
@@ -15,6 +14,7 @@ const GET_CONTRIBUTIONS = gql`
             contributionDays {
               date
               contributionCount
+              color
             }
           }
         }
@@ -23,16 +23,49 @@ const GET_CONTRIBUTIONS = gql`
   }
 `;
 
+const createGrid = (data) => {
+  // Create the grid layout from data
+  const grid = [];
+  data?.user?.contributionsCollection?.contributionCalendar?.weeks?.forEach(
+    (week) => {
+      week.contributionDays.forEach((day) => {
+        grid.push({
+          date: day.date,
+          contributionCount: day.contributionCount,
+          color: day.color,
+        });
+      });
+    }
+  );
+  return grid;
+};
+
 export default function Home() {
   const { loading, error, data } = useQuery(GET_CONTRIBUTIONS);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
+  const gridData = createGrid(data);
+
   return (
     <div>
-      <h1>GitHub Contributions</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <h1>GitHub Snake Game - Contribution Heatmap</h1>
+      <div className="grid">
+        {gridData.map((tile, index) => (
+          <div
+            key={index}
+            className="tile"
+            style={{
+              backgroundColor: tile.color,
+              width: "20px",
+              height: "20px",
+            }}
+          >
+            {/* Tile representing the contribution day */}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
